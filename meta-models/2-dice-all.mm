22b0dd237fa56f428917432d499c01312b4ff825
BnhbTk <53897578+BnhbTk@users.noreply.github.com>
2020-01-14 10:27:36 +0100

    Create 2-dice-all.mm

---
+++ meta-models/2-dice-all.mm
@@ -0,0 +1,44 @@
+//# mdp
+
+
+//constants for the first level (and possibly the second level)
+//# fix const maxS=13;
+//# fix const maxD=6;
+//# fix const maxDec=$maxS-$maxD/2-1;
+
+//constants for the second module
+//# const maxT=30;
+//# const maxDecT=$maxT-$maxD-1;
+
+
+module dice1
+
+//# state s : [0..$maxS] init 0;
+//# state d : [1..$(maxD)] init 1;
+
+//# for{s<=$(maxDec)} [] s=$s -> 0.5 : (s'=?) + 0.5 : (s'=?)
+//# for{s<$maxS & s>=$(maxDec) & d+$(maxDec)=s} [] s=$s -> 0.5 : (s'=$maxS)&(d'=2*$d-1) + 0.5 : (s'=$maxS)&(d'=2*$d);
+//# [] s=$maxS&t=$maxT -> 1 : (s'=$maxS);
+
+endmodule
+
+//# var g:[1..$maxT];
+//# module dice2
+
+//# state t : [0..$maxT] init 0;
+//# state e : [1..$(maxD)] init 1;
+
+
+//# for{t<=$(maxDecT)} [] t=$t -> 0.5 : (t'={g|g<$maxT&g!=$t}) + 0.5 : (t'={g|g<$maxT&g!=$t}) ;
+//# for{t<$maxT & t>=$(maxDecT) & e+$(maxDecT)=t} [] t=$t ->  0.5 : (t'=$maxT)&(e'=$e) + 0.5 : (t'=?)|(t'=$maxT)&(e'=?);
+//# [] s=$maxS&t=$maxT -> 1 : (t'=$maxT);
+
+endmodule
+
+rewards "second_level"
+//# [] !(s=$maxS&t=$maxT) : 1;
+endrewards
+
+rewards "first_level"
+//# [] !(s=$maxS) : 1;
+endrewards
